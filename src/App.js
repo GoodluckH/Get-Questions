5fef2f755ef1453506a2872235df7e5fcc13c129
GoodluckH <lixipu98@gmail.com>
2021-03-17 23:16:48 -0400

    First commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [input, setInput] = useState("");
+  const [questions, setQuestions] = useState([]);
+
+  const div = document.querySelector('.questions')
+  const btn = document.querySelector('.btn')
+
+  const handleChange = (e) => {
+    btn.innerText = "Copy Questions"
+    generateQ(e.target.value)
+    setInput(e.target.value);
+  };
+
+  const generateQ = (s) => {
+    let sentences = s.match(/[^\.!\?]+[\.!\?]+/g);
+
+    if (!sentences) {
+      setQuestions([]);
+      return;
+    }
+
+    let cleanSentences = sentences.map((s) => s.trim());
+    setQuestions(cleanSentences.filter((x) => x.endsWith("?")));
+  };
+
+  const listQuestion = questions.map((q) => {
+    return <p>{q}</p>;
+  });
+
+  
+  const copyText = (e)=>{
+    let text = div.innerText
+    copyToClipBoard(text)
+    e.target.innerText = 'Copied!'
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <textarea
+        onInput={handleChange}
+        data-gramm_editor="false"
+        placeholder="Paste your notes here..."
+        spellcheck="false"
+      ></textarea>
+
+      <button className='btn' onClick={copyText} disabled={!questions.length}>
+        Copy Questions
+      </button>
+      <div className='questions'> {listQuestion}</div>
+     
     </div>
   );
 }
+
+const copyToClipBoard = (str) =>
+{
+    const el = document.createElement('textarea');
+    el.value = str;
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand('copy');
+    document.body.removeChild(el);
+};
 
 export default App;
